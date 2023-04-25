fc94d041a0cd3525f8aabb8be4ee7512a4ed89ed
Aeron01 <one.soulreaper@gmail.com>
2023-04-25 16:33:12 +0200

    change image & tag on click :wip:

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,65 @@
+//let nbr=4;
+let pos=0;
+//let i= pos
+
+const slides = [
+	{
+		"image":"./assets/images/slideshow/slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"./assets/images/slideshow/slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"./assets/images/slideshow/slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"./assets/images/slideshow/slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
 document.body.onload=function(){
-    //let nbr=4;
-    let pos=0;
-    //let i= pos
+    
 
-    banner=document.getElementById("banner")
-    image=banner.querySelector(".banner-img")
-    left=banner.querySelector(".arrow_left")
-    right=banner.querySelector(".arrow_right")
-    dot=banner.querySelector(".dots")
-    checked=banner.querySelector(".dot_selected")
-
+    banner=document.getElementById("banner");
+    image=banner.querySelector(".banner-img");
+    left=banner.querySelector(".arrow_left");
+    right=banner.querySelector(".arrow_right");
+    dot=banner.querySelector(".dots");
+    checked=banner.querySelector(".dot_selected");
+    tag=banner.querySelector("p")
     //span=document.createElement("span" + "src="[image]);
     //alt=
     
     
 
+
     function sliders(){
-        for (i=0; i < slides.length; i++) {
+        for (i = 0; i < slides.length; i++) {
             
-            if (pos > slides.length){
+            if (pos > slides.length-1 ){
                 pos=0
             }
             else if (pos < 0) {
-            pos=slides.length
+                pos=slides.length-1
             }
         }
         console.log(pos)
+        console.log(slides[pos].image)
+        console.log(tag)
+        console.log(slides[pos].tagLine)
+
+        image.setAttribute("src", slides[pos].image);
+        tag.innerHTML = slides[pos].tagLine
     }
 
     right.onclick=()=> {
         if (right.onclick) {
         pos=pos+1
+        
             sliders()
             /*for (i=0; i < slides.length; i++) {
                 if(pos > slides.length) {
@@ -44,6 +73,7 @@ document.body.onload=function(){
     left.onclick=()=> {
         if (right.onclick) {
         pos=pos-1
+        
             sliders()
             /*if (pos < 0) {
                 pos=slides.length
@@ -55,22 +85,6 @@ document.body.onload=function(){
 }
 
 
-
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+/*for (let createDots of slides.length) {
+    document.createElement()
+}*/
