a81f0e317d01ce7ef83aa798f2fae593c8c3cbb4
Aeron01 <one.soulreaper@gmail.com>
2023-04-27 14:07:10 +0200

    test branch

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+window.onload=()=>{
 //---- Variables
 let pos=0;
 
@@ -6,7 +7,6 @@ const image=banner.querySelector(".banner-img");
 const left=banner.querySelector(".arrow_left");
 const right=banner.querySelector(".arrow_right");
 const dots=banner.querySelector(".dots");
-const checked=banner.querySelector(".dot_selected");
 const tag=banner.querySelector("p");
 
 
@@ -32,27 +32,41 @@ const slides = [
 ]
 
 //---- Dots creation
+
+
 slides.forEach((_, index) => {
-    const span=document.createElement("span");
-    span.onclick=()=>{
-        setSlidesPosition(index)
-    }
-    span.classList.add("dot");
-    dots.appendChild(span);
+    dots.innerHTML+=`<span class="dot" onclick="setSlidesPosition(${index})"></span>` // eviter d'utiliser le innerHTML dans des formulaires. risque de sécuriter par injection de code(ont peut le contrer en ne prenant pas en compte les caratères spéciaux utilisé pour le html (ex : /\< et >)))
 });
 
+
+
+
+//--- Listener & pos change
+
+
+right.onclick=()=> {
+    if (right.onclick) {
+        setSlidesPosition(pos+1)            
+    }
+}
+
+left.onclick=()=> {
+    if (right.onclick) {
+        setSlidesPosition(pos-1)
+    }
+}
+
+setSlidesPosition(0);
 
 
 //---- Functions
 function setSlidesPosition(newPos){
     
         
-    if (newPos > slides.length-1 ){
-        newPos=0;
-    }
-    else if (newPos < 0) {
-        newPos=slides.length-1;
-    }
+    (newPos > slides.length-1 ) && (newPos=0); //idem que if else en plus court (utilise le systeme ternaire (booleen))
+    
+    (newPos < 0) && (newPos=slides.length-1);
+
     
     pos=newPos;
 
@@ -69,22 +83,4 @@ function setSlidesPosition(newPos){
     });
 }
 
-//--- Listener & pos change
-document.body.onload=function(){
-
-    right.onclick=()=> {
-        if (right.onclick) {
-            setSlidesPosition(pos+1)            
-        }
-    }
-
-    left.onclick=()=> {
-        if (right.onclick) {
-            setSlidesPosition(pos-1)
-        }
-    }
-
-    setSlidesPosition(0);
 }
-
-
